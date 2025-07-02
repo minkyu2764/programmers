01f1a4f667dafed4269e1d742688314c0a0e983e
Kimminkyu <162242603+minkyu2764@users.noreply.github.com>
2025-07-02 16:09:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.95 ms, Memory: 4.28 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.c
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.c
@@ -9,8 +9,6 @@ char* solution(int n) {
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
             strcat(answer, "수"); 
-            // 한글은 1바이트보다 커서 오류 발생. 즉, 문자열에 덧붙이는 작업이 필요하므로 strcat을 사용
-                                
         } else {
             strcat(answer, "박");
         }
