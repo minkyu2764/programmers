74f3e0b44b4d82627b5f23fc630f44542a9cffb9
Kimminkyu <162242603+minkyu2764@users.noreply.github.com>
2025-07-02 15:40:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int count = 0;
+
+    while (num != 1 && count < 500) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else if (num % 2 == 1) {
+            num = num * 3 + 1;
+        }
+        count++;
+    }
+
+    if (count == 500) {
+        return -1;
+    }
+
+    return count;
+}
