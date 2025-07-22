60071bad4cdb4d4bd8751cb6847cece8f972d840
Kimminkyu <162242603+minkyu2764@users.noreply.github.com>
2025-07-22 15:15:52 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    int odd_sum = 0;
+    int even_sum = 0;
+    
+    for (int i = 0; i < num_list_len; i++) {
+        if ((i+1) % 2 == 0) {
+            odd_sum += num_list[i];
+        }
+        else if ((i+1) % 2 == 1) {
+            even_sum += num_list[i];
+        }
+    }
+    answer = (odd_sum >= even_sum) ? odd_sum : even_sum;
+    
+    return answer;
+}
